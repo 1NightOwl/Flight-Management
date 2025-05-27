7bdfc9dc0347d9d1f8ca08219eb08d643978e23b
Eris Kokedhima <User@DESKTOP-DQASLH0>
2025-05-27 16:47:55 +0200

    krijimi i biletave

---
+++ Migrations/20250526120344_ShtoKlasaNeBileta.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FlightManagement.Migrations
+{
+    /// <inheritdoc />
+    public partial class ShtoKlasaNeBileta : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Klasa",
+                table: "Biletat",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Klasa",
+                table: "Biletat");
+        }
+    }
+}
